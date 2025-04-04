26c9498668d099c3bacefa11cfe0a92b7e2e940c
Mohammed1Abdou <piano.abdou@hotmail.com>
2025-04-04 14:39:20 -0400

    Ajouter des classes

---
+++ src/administration/Centrale.java
@@ -0,0 +1,14 @@
+package administration;
+
+import moyenTransport.Voiture;
+
+public class Centrale {
+    Repartiteur repartitateur;
+
+     public void communique(Voiture v)
+    {
+
+
+
+    }
+}
---
+++ src/moyenTransport/Voiture.java
@@ -0,0 +1,26 @@
+package moyenTransport;
+
+public class Voiture {
+    Moteur m;
+    int nbPlaces;
+    double prix;
+    int getNbPlaces()
+    {
+
+       return nbPlaces;
+    }
+
+    double calculerConsommationessence()
+    {
+
+        return 24;
+    }
+    static int getNbRoues()
+    {
+        return 4;
+
+    }
+
+
+
+}
